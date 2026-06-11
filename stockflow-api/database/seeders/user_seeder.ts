a238d91735456e1e97916a8961199660262582b6
pedrolucasods <ploliveira1805@gmail.com>
2026-06-10 21:02:19 -0400

    fix: fix the faker importation

--- stockflow-api/database/seeders/user_seeder.ts
+++ stockflow-api/database/seeders/user_seeder.ts
@@ -1,6 +1,6 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import User from '#models/user'
-import faker from 'faker-br'
+import {faker} from '@faker-js/faker'
 export default class extends BaseSeeder {
   async run() {
     const UserRoles = {
@@ -8,7 +8,7 @@ export default class extends BaseSeeder {
       USER: 'user'
     }
     // Write your database queries inside the run method
-    for(let i=0; i<3;i++){
+    for(let i=0; i<15;i++){
       await User.create({
         name:`${faker.person.firstName()} ${faker.person.lastName()} `,
         email:faker.internet.email(),
