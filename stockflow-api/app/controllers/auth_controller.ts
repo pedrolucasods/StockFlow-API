feade6c290aa25541364ef58fbe4d854f44586d8
Pedro Lucas <ploliveira1805@gmail.com>
2026-06-11 22:13:13 -0400

    feat: add forgot password functionality (#32)

--- stockflow-api/app/controllers/auth_controller.ts
+++ stockflow-api/app/controllers/auth_controller.ts
@@ -1,8 +1,9 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { ValidadorLogin } from '#validators/auth/login_validator'
 import { ValidadorRegister } from '#validators/auth/register_validator'
+import { ValidatorForgotPassword } from '#validators/auth/forgot_password_validator'
 import { AuthService } from '#services/auth/auth_service'
-
+import { PasswordResetService } from '#services/password_reset_service'
 
 export default class AuthController {
     async login({request, auth}:HttpContext){
@@ -16,5 +17,11 @@ export default class AuthController {
         const data = await request.validateUsing(ValidadorRegister)
         const user = await AuthService.register(data)
         return {user} 
+    }
+
+    async forgot_password({request}:HttpContext){
+        const data = await request.validateUsing(ValidatorForgotPassword)
+        const token_reset = await PasswordResetService.create_code_reset(data.email)
+        return ({message:"Recovery code sent"})
     }
 }
